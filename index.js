26c536f9beda458308a7367c378831cc6a07aed6
todocono <todocono@gmail.com>
2018-05-01 12:37:29 +0800

    changed npm ver

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const server = http.createServer((req, res) => {
   res.end('Hello Rudi\n');
 });
 server.listen(port, hostname, () => {
-  console.log(`Server running at ${port}/`);
+  console.log(`Server running at ${PORT}/`);
 });
